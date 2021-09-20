77d278f4cc80a55561f859502609ea60a542bb61
PatrickMogul <patrick@easynode.one>
2021-09-20 17:36:49 -0500

    unused import

--- toolbox/start.py
+++ toolbox/start.py
@@ -3,7 +3,7 @@ import pathlib
 import time
 from os import environ
 from utils.installer import *
-from utils.shared import loaderIntro, getValidatorInfo, loadVarFile
+from utils.shared import loaderIntro, loadVarFile
 
 
 if __name__ == "__main__":
