5154a737a12d866d417368c5ed7d11135fb236f9
patrickmogul <patrick@easynode.one>
2021-09-20 23:36:21 +0000

    v1.0.6

--- toolbox/utils/installer.py
+++ toolbox/utils/installer.py
@@ -7,7 +7,7 @@ from colorama import Fore, Style
 from utils.shared import setAPIPaths, getShardMenu, getExpressStatus, setMainOrTest, getNodeType, setWalletEnv, process_command, printStars, printStarsReset, printWhiteSpace, askYesNo, save_text, installHarmonyApp, installHmyApp
 
 
-easyVersion = "1.0.5"
+easyVersion = "1.0.6"
 serverHostName = socket.gethostname()
 userHomeDir = os.path.expanduser("~")
 dotenv_file = f"{userHomeDir}/.easynode.env"
