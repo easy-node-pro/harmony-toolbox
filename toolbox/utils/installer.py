f354d53ec32bf257e9472e2c54d19207e99e8df5
Patrick Mogul <patrick@easynode.one>
2021-09-20 01:37:02 -0500

    v1.0.1

--- toolbox/utils/installer.py
+++ toolbox/utils/installer.py
@@ -9,7 +9,7 @@ from utils.shared import isFirstRun, setAPIPaths, getShardMenu, getExpressStatus
 from utils.toolbox import runRegularNode, runFullNode
 
 
-easyVersion = "1.0.0"
+easyVersion = "1.0.1"
 serverHostName = socket.gethostname()
 userHomeDir = os.path.expanduser("~")
 dotenv_file = f"{userHomeDir}/.easynode.env"
