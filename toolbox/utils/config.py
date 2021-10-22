31cc4d56e8820249a6bea981fb4045a2f1dc455f
PatrickMogul <patrick@easynode.one>
2021-10-22 17:35:04 -0500

    Fix collect

--- toolbox/utils/config.py
+++ toolbox/utils/config.py
@@ -3,7 +3,7 @@ import socket
 import urllib.request
 
 class validatorToolbox:
-    easyVersion = "1.2.3"
+    easyVersion = "1.2.4"
     serverHostName = socket.gethostname()
     userHomeDir = os.path.expanduser("~")
     dotenv_file = f"{userHomeDir}/.easynode.env"
