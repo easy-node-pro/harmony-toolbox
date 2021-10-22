b594b1c91208d3749a20f4af2e417e2377708fbe
PatrickMogul <patrick@easynode.one>
2021-10-22 14:11:54 -0500

    Cleanup

--- toolbox/utils/config.py
+++ toolbox/utils/config.py
@@ -19,10 +19,6 @@ class validatorToolbox:
     validatorData = os.path.join(toolboxLocation, "toolbox", "metadata", "validator.json")
     passwordPath = os.path.join(harmonyDirPath, "passphrase.txt")
     toolboxLocation = os.path.join(userHomeDir, "validatortoolbox")
-    main_net_rpc = 'https://rpc.s0.t.hmny.io'
-    main_net_call = '/home/serviceharmony/harmony/hmy --node="https://api.s0.t.hmny.io"'
-    test_net_rpc = 'https://rpc.s0.b.hmny.io'
-    test_net_call = '/home/serviceharmony/harmony/hmy --node="https://api.s0.b.hmny.io"'
     ourExternalIPAddress = urllib.request.urlopen("https://ident.me").read().decode("utf8")
     mainMenuRegular = os.path.join(toolboxLocation, "toolbox", "messages", "regularmenu.txt")
     mainMenuFull = os.path.join(toolboxLocation, "toolbox", "messages", "fullmenu.txt")
