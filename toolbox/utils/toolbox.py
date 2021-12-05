639eac54f75a9c91900f952918a6491309b7039b
PatrickMogul <patrick@easynode.one>
2021-12-05 16:27:30 -0600

    anyuser collect

--- toolbox/utils/toolbox.py
+++ toolbox/utils/toolbox.py
@@ -31,7 +31,7 @@ def rewardsCollector() -> None:
         f"*\n* For your validator wallet {environ.get('VALIDATOR_WALLET')}\n* You have {getRewardsBalance(validatorToolbox.rpc_endpoints, environ.get('VALIDATOR_WALLET'))} $ONE pending.\n* Would you like to collect your rewards on the Harmony mainnet? (YES/NO) "
     )
     if question:
-        collectRewards("/home/serviceharmony/harmony/hmy --node=\'https://api.s0.t.hmny.io\' ")
+        collectRewards(f"/home/{activeUserName}/harmony/hmy --node=\'https://api.s0.t.hmny.io\' ")
         printStars()
         print(
             Fore.GREEN + f"* mainnet rewards for {environ.get('VALIDATOR_WALLET')} have been collected." + Style.RESET_ALL
