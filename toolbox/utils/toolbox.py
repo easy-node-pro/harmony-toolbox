12ac66e73b76af0d6f8f667cb95ce3d240d2dda2
PatrickMogul <patrick@easynode.one>
2021-12-10 09:40:46 -0600

    just use f'{validatorToolbox.hmyAppPath}'

--- toolbox/utils/toolbox.py
+++ toolbox/utils/toolbox.py
@@ -514,13 +514,13 @@ def upgradeHarmonyApp(testOrMain):
 def runStats() -> str:
     timeNow = datetime.now()
     ourShard = environ.get("SHARD")
-    remote_shard_0 = [f'/home/{validatorToolbox.activeUserName}/harmony/hmy', 'blockchain', 'latest-headers', f'--node=https://api.s0.t.hmny.io']
+    remote_shard_0 = [f'{validatorToolbox.hmyAppPath}', 'blockchain', 'latest-headers', f'--node=https://api.s0.t.hmny.io']
     result_remote_shard_0 = run(remote_shard_0, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     remote_data_shard_0 = json.loads(result_remote_shard_0.stdout)
-    remote_shard = [f'/home/{validatorToolbox.activeUserName}/harmony/hmy', 'blockchain', 'latest-headers', f'--node=https://api.s{ourShard}.t.hmny.io']
+    remote_shard = [f'{validatorToolbox.hmyAppPath}', 'blockchain', 'latest-headers', f'--node=https://api.s{ourShard}.t.hmny.io']
     result_remote_shard = run(remote_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     remote_data_shard = json.loads(result_remote_shard.stdout)
-    local_shard = [f'/home/{validatorToolbox.activeUserName}/harmony/hmy', 'blockchain', 'latest-headers']
+    local_shard = [f'{validatorToolbox.hmyAppPath}', 'blockchain', 'latest-headers']
     result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     local_data_shard = json.loads(result_local_shard.stdout)
     print(f"""
