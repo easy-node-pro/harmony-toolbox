038d499ed70633cc489b5471c1437a3c415d3286
PatrickMogul <patrick@easynode.one>
2022-02-06 11:15:04 -0600

    update option 1 to use env's

--- toolbox/utils/config.py
+++ toolbox/utils/config.py
@@ -3,7 +3,7 @@ import socket
 import urllib.request
 
 class validatorToolbox:
-    easyVersion = "1.2.9"
+    easyVersion = "1.2.10"
     serverHostName = socket.gethostname()
     userHomeDir = os.path.expanduser("~")
     dotenv_file = f"{userHomeDir}/.easynode.env"
