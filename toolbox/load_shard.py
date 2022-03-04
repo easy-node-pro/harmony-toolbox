2450a5996c1268426e1c1c43fc9309081764f677
PatrickMogul <patrick@easynode.one>
2022-03-04 11:25:27 -0600

    No shard? Ask & set.

--- toolbox/load_shard.py
+++ toolbox/load_shard.py
@@ -2,12 +2,14 @@ import os
 from os import environ
 from utils.config import validatorToolbox
 from utils.installer import cloneShards, loadVarFile
-from utils.shared import loaderIntro
+from utils.shared import loaderIntro, getShardMenu
 
 if __name__ == "__main__":
     os.system("clear")
     loaderIntro()
     loadVarFile()
+    if not environ.get('SHARD'):
+        getShardMenu(validatorToolbox.dotenv_file)
     print(
         "* Harmony ONE Validator Shard Downloader"
     )
