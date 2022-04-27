c54f44abc655c7a89651c797dafeb7bb9d806ba8
PatrickMogul <patrick@easynode.one>
2022-04-27 13:17:45 -0500

    stats check error remote/local and msg

--- toolbox/utils/toolbox.py
+++ toolbox/utils/toolbox.py
@@ -518,22 +518,35 @@ def runStats() -> str:
     result_remote_shard_0 = run(remote_shard_0, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     remote_data_shard_0 = json.loads(result_remote_shard_0.stdout)
     remote_shard = [f'{validatorToolbox.hmyAppPath}', 'blockchain', 'latest-headers', f'--node=https://api.s{ourShard}.{ourNetwork}.hmny.io']
-    result_remote_shard = run(remote_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    remote_data_shard = json.loads(result_remote_shard.stdout)
-    local_shard = [f'{validatorToolbox.hmyAppPath}', 'blockchain', 'latest-headers']
-    result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     try:
-        local_data_shard = json.loads(result_local_shard.stdout)
+        result_remote_shard = run(remote_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        remote_data_shard = json.loads(result_remote_shard.stdout)
     except (ValueError, KeyError, TypeError):
-        {stringStars()}
         print(f"""
+        {stringStars()}
         * Current Date & Time: {timeNow}
         *
-        """)
         {stringStars()}
+        """)
         shardStats(ourShard)
         {stringStars()}
-        input(f"No local data detected, did you just restart Harmony? Press ENTER to return to the main menu.")
+        input(f"* No remote data returned, RPC may be unresponsive. Wait few moments and try again. Press ENTER to return to the main menu.")
+        {stringStars()}
+        return
+    local_shard = [f'{validatorToolbox.hmyAppPath}', 'blockchain', 'latest-headers']
+    try:
+        result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        local_data_shard = json.loads(result_local_shard.stdout)
+    except (ValueError, KeyError, TypeError):
+        print(f"""
+        {stringStars()}
+        * Current Date & Time: {timeNow}
+        *
+        {stringStars()}
+        """)
+        shardStats(ourShard)
+        {stringStars()}
+        input(f"* No local data detected, did you just restart Harmony? Wait a few moments and try again. Press ENTER to return to the main menu.")
         {stringStars()}
         return
     print(f"""
