5136d434d0b3f7d8f4354a5bf33b340ec8009920
PatrickMogul <patrick@easynode.one>
2022-05-26 03:21:41 -0500

    get ip failing for ssl

--- toolbox/utils/config.py
+++ toolbox/utils/config.py
@@ -2,8 +2,15 @@ import os
 import socket
 import urllib.request
 
+def getUrl() -> None:
+    try:
+        result = urllib.request.urlopen("https://ident.me").read().decode("utf8")
+    except (ValueError, KeyError, TypeError):
+        return '0.0.0.0'
+    return result
+
 class validatorToolbox:
-    easyVersion = "1.3.3"
+    easyVersion = "1.3.5"
     serverHostName = socket.gethostname()
     userHomeDir = os.path.expanduser("~")
     dotenv_file = f"{userHomeDir}/.easynode.env"
@@ -19,7 +26,7 @@ class validatorToolbox:
     validatorData = os.path.join(toolboxLocation, "toolbox", "metadata", "validator.json")
     passwordPath = os.path.join(harmonyDirPath, "passphrase.txt")
     toolboxLocation = os.path.join(userHomeDir, "validatortoolbox")
-    ourExternalIPAddress = urllib.request.urlopen("https://ident.me").read().decode("utf8")
+    ourExternalIPAddress = getUrl()
     mainMenuRegular = os.path.join(toolboxLocation, "toolbox", "messages", "regularmenu.txt")
     mainMenuFull = os.path.join(toolboxLocation, "toolbox", "messages", "fullmenu.txt")
     rpc_endpoints = ['https://api.s0.t.hmny.io', 'https://api.harmony.one', 'https://harmony-0-rpc.gateway.pokt.network']
