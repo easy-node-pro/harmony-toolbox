1fba4d1f3d8a3c142aa95ccc97603ad08d0b800b
PatrickMogul <patrick@easynode.one>
2022-08-01 12:29:39 -0500

    text

--- toolbox/menu.py
+++ toolbox/menu.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     os.system("clear")
     loaderIntro()
     if not os.path.exists(validatorToolbox.dotenv_file):
-        print("Install Harmony First!!!\nRun python3 toolbox/install.py")
+        print("Install Harmony First!!!\nRun python3 ~/validatortoolbox/toolbox/install.py")
         raise SystemExit(0)
     loadVarFile()
     if not environ.get("VALIDATOR_WALLET"):
