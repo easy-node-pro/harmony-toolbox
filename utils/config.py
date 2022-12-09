38f163de62726514de1d37799598ded5daa8ca75
PatrickMogul <patrick@easynode.pro>
2022-12-09 17:09:56 -0600

    on the move

--- utils/config.py
+++ utils/config.py
@@ -12,7 +12,7 @@ def getUrl() -> None:
     return result
 
 class validatorToolbox:
-    easyVersion = "1.6.9"
+    easyVersion = "1.7.0"
     serverHostName = socket.gethostname()
     userHomeDir = os.path.expanduser("~")
     dotenv_file = f"{userHomeDir}/.easynode.env"
