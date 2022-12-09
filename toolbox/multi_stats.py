8b637a738d73ab5032f4e526fd73a117723a5cd9
PatrickMogul <patrick@easynode.pro>
2022-12-09 12:45:58 -0600

    Tester

--- toolbox/multi_stats.py
+++ toolbox/multi_stats.py
@@ -62,7 +62,7 @@ def statsOutputRegular() -> None:
     printStars()
     while count < 4:
         try:
-            remote_data, local_data = multiValidatorStats()
+            remote_data, local_data = multiValidatorStats(count)
             print(f"* Remote Shard {count} Epoch: {remote_data['result']['shard-chain-header']['epoch']}, Current Block: {literal_eval(remote_data['result']['shard-chain-header']['number'])}")
             print(f"*  Local Shard {count} Epoch: {local_data['result']['shard-chain-header']['epoch']}, Current Block: {literal_eval(local_data['result']['shard-chain-header']['number'])}, Local Shard {environ.get('SHARD')} Size: {getDBSize(environ.get('SHARD'))}")
             printStars()
