c3bfb57c1088e55e9dd766b3eb21254de2c9d8f5
PatrickMogul <patrick@easynode.pro>
2022-12-14 01:59:06 -0600

    str

--- src/toolbox/toolbox.py
+++ src/toolbox/toolbox.py
@@ -358,7 +358,7 @@ def run_regular_node(software_versions) -> None:
             else:
                 subprocess.run("clear")
                 print_stars()
-                menu_options[option[0]]()
+                menu_options[str(option[0])]()
                 if option != 1:
                     refresh_stats(1)
         else:
