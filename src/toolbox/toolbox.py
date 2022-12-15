22b76ea0c7aaf6bde58eceac3226ea2fcfcd494f
PatrickMogul <patrick@easynode.pro>
2022-12-15 01:02:34 -0600

    Better

--- src/toolbox/toolbox.py
+++ src/toolbox/toolbox.py
@@ -365,7 +365,9 @@ def run_regular_node(software_versions) -> None:
                         refresh_stats(1)
             except KeyError:
                 print(f'* Bad option, try again. Press enter to continue.')
+                print_stars()
                 input()
+                refresh_stats(1)
         else:
             try:
                 option, timedOut = timedInteger("* Auto refresh disabled, Enter your menu choice: ", timeout=-1, resetOnInput=True, allowNegative=False)
@@ -376,7 +378,9 @@ def run_regular_node(software_versions) -> None:
                     refresh_stats(1)
             except KeyError:
                 print(f"* Bad option, try again. Press enter to continue.")
+                print_stars()
                 input()
+                refresh_stats(1)
 
 def harmony_service_status() -> None:
     status = subprocess.call(["systemctl", "is-active", "--quiet", "harmony"])
