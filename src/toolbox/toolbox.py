39675ba77218c297a65f37d8965d4e8bbb8dc801
patrickmogul <patrick@easynode.pro>
2022-12-28 19:22:22 -0600

    stats testing

--- src/toolbox/toolbox.py
+++ src/toolbox/toolbox.py
@@ -477,14 +477,14 @@ def menu_validator_stats():
         "latest-headers",
         f'--node=https://api.s0.{environ.get("NETWORK_SWITCH")}.hmny.io',
     ]
-    result_remote_shard_0 = run(remote_shard_0, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     try:
+        result_remote_shard_0 = run(remote_shard_0, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         remote_data_shard_0 = json.loads(result_remote_shard_0.stdout)
     except:
         print(f'* Remote Shard 0 Offline')
-    local_shard = [f"{easy_env.hmy_app}", "blockchain", "latest-headers"]
-    result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     try:
+        local_shard = [f"{easy_env.hmy_app}", "blockchain", "latest-headers"]
+        result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         local_data_shard = json.loads(result_local_shard.stdout)
     except:
         print(f'* Local Server Offline, restart your service or troubleshoot the issue by running the following in your ~/harmony directory:\n* ./harmony -c harmony.conf')
