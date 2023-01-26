2410daa8eb0757806028d17ef3e28031e900d20b
patrickmogul <patrick@easynode.pro>
2023-01-26 13:22:24 -0600

    v1.0.4 bugfix

--- src/toolbox/config.py
+++ src/toolbox/config.py
@@ -13,7 +13,7 @@ def get_url() -> None:
     return result
 
 class easy_env:
-    easy_version = "1.0.3"
+    easy_version = "1.0.4"
     server_host_name = socket.gethostname()
     user_home_dir = os.path.expanduser("~")
     dotenv_file = f"{user_home_dir}/.easynode.env"
