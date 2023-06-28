9bbdcd56a0ceb6217cd64ded3a424301969b2901
patrickmogul <patrick@easynode.pro>
2023-06-28 11:02:33 -0500

    Test

--- src/toolbox/library.py
+++ src/toolbox/library.py
@@ -489,8 +489,8 @@ def wallet_pending_rewards(wallet):
 
 
 def get_sign_pct() -> str:
-    rpc_endpoint = EnvironmentVariables().working_rpc_endpoint
-    hmy_external_rpc = f"{EnvironmentVariables.hmy_app} --node='{rpc_endpoint}'"
+    config = EnvironmentVariables()
+    hmy_external_rpc = f"{EnvironmentVariables.hmy_app} --node='{config.working_rpc_endpoint}'"
     output = subprocess.getoutput(
         f"{hmy_external_rpc} blockchain validator information {environ.get('VALIDATOR_WALLET')} | grep signing-percentage"
     )
