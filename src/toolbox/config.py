4672bc28a3c2915523aff71cb9369c2788f1af25
patrickmogul <patrick@easynode.pro>
2023-06-28 10:51:04 -0500

    Test

--- src/toolbox/config.py
+++ src/toolbox/config.py
@@ -1,22 +1,20 @@
 import os
 import socket
 import urllib.request
+import requests
 from os import environ
 from concurrent.futures import ThreadPoolExecutor, TimeoutError
 
 def get_url(timeout=5) -> str:
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        future = executor.submit(urllib.request.urlopen, "https://ident.me")
-        try:
-            response = future.result(timeout)
-            result = response.read().decode("utf8")
-        except TimeoutError:
-            print("Request timed out.")
-            result = '0.0.0.0'
-        except Exception as x:
-            print(type(x), x)
-            result = '0.0.0.0'
+    try:
+        response = requests.get("https://ident.me", timeout=timeout)
+        response.raise_for_status()  # Raises a HTTPError if the response was unsuccessful
+        result = response.text
+    except requests.exceptions.RequestException as x:
+        print(type(x), x)
+        result = '0.0.0.0'
     return result
+
 
 class EnvironmentVariables:
     easy_version = "1.0.6"
@@ -38,12 +36,11 @@ class EnvironmentVariables:
     main_menu_regular = os.path.join(toolbox_location, "src", "messages", "regularmenu.txt")
     main_menu_full = os.path.join(toolbox_location, "src", "messages", "fullmenu.txt")
     rpc_endpoints = ['https://api.s0.t.hmny.io', 'https://api.harmony.one', 'https://rpc.ankr.com/harmony']
-    rpc_endpoints_test = ['https://rpc.s0.b.hmny.io', 'https://api.s0.pops.one']
     rpc_endpoints_max_connection_retries = 10
     hmy_tmp_path = '/tmp/hmy'
     harmony_tmp_path = '/tmp/harmony'
     
-    @classmethod
+    @staticmethod
     def get_working_endpoint(cls, endpoints):
         for endpoint in endpoints:
             try:
@@ -55,11 +52,5 @@ class EnvironmentVariables:
         return None  # If we get here, none of the endpoints worked
 
     @property
-    @classmethod
     def working_rpc_endpoint(cls):
         return cls.get_working_endpoint(cls.rpc_endpoints)
-
-    @property
-    @classmethod
-    def working_rpc_endpoint_test(cls):
-        return cls.get_working_endpoint(cls.rpc_endpoints_test)
