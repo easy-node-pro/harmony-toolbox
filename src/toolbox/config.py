0d42af190b3ec990ae9b748f4844fb0e56ed6a4f
patrickmogul <patrick@easynode.pro>
2023-06-30 10:08:50 -0500

    1.0.8 more flags

--- src/toolbox/config.py
+++ src/toolbox/config.py
@@ -17,7 +17,7 @@ def get_url(timeout=5) -> str:
 
 
 class EnvironmentVariables:
-    easy_version = "1.0.7"
+    easy_version = "1.0.8"
     server_host_name = socket.gethostname()
     user_home_dir = os.path.expanduser("~")
     dotenv_file = f"{user_home_dir}/.easynode.env"
