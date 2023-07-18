60c8b5d7754b717eff7e6ba6f9138090ca8315ca
patrickmogul <patrick@easynode.pro>
2023-07-17 20:52:04 -0500

    Better

--- src/install.py
+++ src/install.py
@@ -1,12 +1,14 @@
-import subprocess
+import subprocess, os
 from toolbox.library import loader_intro, print_stars
-
-# This is just a temp file to replace start.py to guide people to harmony.sh. 
 
 if __name__ == "__main__":
     subprocess.run("clear")
     loader_intro()
     subprocess.run("clear")
     print_stars()
-    print("* We have moved to using harmony.sh to launch our toolbox and get stats.\n*\n* Run the following to download and setup the new harmony.sh file:\n*\n* cd ~/ && wget -O harmony.sh https://raw.githubusercontent.com/easy-node-pro/harmony-toolbox/main/src/bin/harmony.sh && chmod +x harmony.sh\n*\n* Now you can launch the toolbox with ./harmony.sh or get just stats with ./harmony.sh -s\n* See ./harmony.sh -h for help!\n*")    
+    if not os.path.exists("~/harmony.sh"):
+        print("* Downloading harmony.sh to ~/")
+        subprocess.run("cd ~/ && wget -O harmony.sh https://raw.githubusercontent.com/easy-node-pro/harmony-toolbox/main/src/bin/harmony.sh && chmod +x harmony.sh", shell=True)
+    else:
+        print("* harmony.sh already exists in ~/\n*\n* This will exit, please run ~/harmony.sh to launch the toolbox.\n*")
     print_stars()
