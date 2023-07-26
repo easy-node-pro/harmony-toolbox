9024c1aad3d17887fed320fd7615e54b9334c33c
patrickmogul <patrick@easynode.pro>
2023-07-25 20:06:15 -0500

    move parse

--- src/menu.py
+++ src/menu.py
@@ -9,13 +9,13 @@ load_var_file(EnvironmentVariables.dotenv_file)
 def app():
     # Check for old toolbox first
     old_toolbox_check()
-    # Run parser if flags added
-    parser = argparse.ArgumentParser(description="Harmony Validator Toolbox - Help Menu by EasyNode.pro")
-    parse_flags(parser)
     # passed .env check, let's load it!
     first_env_check(EnvironmentVariables.dotenv_file)
     # This section is for hard coding new settings for current users.
     safety_defaults()
+    # Run parser if flags added
+    parser = argparse.ArgumentParser(description="Harmony Validator Toolbox - Help Menu by EasyNode.pro")
+    parse_flags(parser)
     # Run regular validator node
     start_regular_node()
 
