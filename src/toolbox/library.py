2e105374a1988dd25c6e080fbff4110d7aa04409
patrickmogul <patrick@easynode.pro>
2023-08-04 15:18:19 -0500

    Popen

--- src/toolbox/library.py
+++ src/toolbox/library.py
@@ -414,7 +414,7 @@ def passphrase_set():
 
 
 def process_command(command: str, shell=True, print_output=True) -> bool:
-    result = subprocess.run(command, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result = subprocess.Popen(command, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     if print_output and result.stdout:
         print(result.stdout)
