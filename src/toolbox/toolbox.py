3ea2c88352c753b8f96d6fc4844093fa6bccfac7
patrickmogul <patrick@easynode.pro>
2023-08-04 14:55:55 -0500

    Text

--- src/toolbox/toolbox.py
+++ src/toolbox/toolbox.py
@@ -128,19 +128,18 @@ def send_rewards(networkCall, sendAmount, rewards_wallet):
     
     
 def send_rewards_func(suggested_send, validator_wallet_balance, rewards_wallet, validator_wallet, bypass = False):
-    print("*\n*\n")
-    print_stars()
-    print("\n* Send your Harmony ONE Rewards?")
-    print_stars()
     if bypass == False:
+        print("*\n*\n")
+        print_stars()
+        print("\n* Send your Harmony ONE Rewards?")
+        print_stars()
         question = ask_yes_no(
             f"* You have {validator_wallet_balance} $ONE available to send. We suggest sending {suggested_send} $ONE using your reservation settings.\n* Would you like to send {suggested_send} $ONE to {rewards_wallet} now? (YES/NO)"
         )
-        if question:
-            print("*\n*\n* Sending your Harmony ONE Rewards, awaiting confirmation...\n")
-        else: 
+        if question == False: 
             print("*\n*\n* Skipping sending of rewards.\n")
             return
+    print("*\n*\n* Sending your Harmony ONE Rewards, awaiting confirmation...\n")
     send_rewards(EnvironmentVariables.hmy_app, suggested_send, rewards_wallet)
     validator_wallet_balance = get_wallet_balance(validator_wallet)
     rewards_wallet_balance = get_wallet_balance(rewards_wallet)
