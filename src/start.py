41b9c70258324e71bc0fc944a3bb67f6032e2910
patrickmogul <patrick@easynode.pro>
2023-08-04 19:44:35 -0500

    no os

--- src/start.py
+++ src/start.py
@@ -1,19 +1,19 @@
-import subprocess, os
+import subprocess
+from pathlib import Path
 from toolbox.config import EnvironmentVariables
 from toolbox.library import loader_intro, print_stars
 
 if __name__ == "__main__":
     loader_intro()
-    subprocess.run("clear")
+    subprocess.run(["clear"])
     print_stars()
-    if os.path.isfile(EnvironmentVariables.user_home_dir + "/harmony.sh"):
-        print(
-            "* harmony.sh already exists in ~/\n*\n* This will exit, please change to your home directory and run ./harmony.sh to launch the toolbox.\n*\n* Run ./harmony.sh -h for our new help menu!\n*"
-        )
+    harmony_script_path = Path(EnvironmentVariables.user_home_dir) / "harmony.sh"
+    if harmony_script_path.is_file():
+        print("* harmony.sh already exists in ~/\n*\n* Launching the toolbox with help menu.")
+        subprocess.run(["bash", "-x", str(harmony_script_path), "-h"])
     else:
         print("* Downloading harmony.sh to ~/")
-        subprocess.run(
-            "cd ~/ && wget -O harmony.sh https://raw.githubusercontent.com/easy-node-pro/harmony-toolbox/main/src/bin/harmony.sh && chmod +x harmony.sh && ./harmony.sh",
-            shell=True,
-        )
+        subprocess.run(["wget", "-O", str(harmony_script_path), "https://raw.githubusercontent.com/easy-node-pro/harmony-toolbox/main/src/bin/harmony.sh"])
+        harmony_script_path.chmod(0o755)
+        subprocess.run([str(harmony_script_path)])
     print_stars()
