92d8eff5514747707ab228de5353291e49f99989
patrickmogul <patrick@easynode.pro>
2023-08-06 00:43:04 -0500

    Formatting

--- src/multi_stats.py
+++ src/multi_stats.py
@@ -7,9 +7,9 @@ from toolbox.library import (
     loader_intro,
     finish_node,
     refreshing_stats_message,
-    set_network, 
+    set_network,
     get_folders,
-    validator_stats_output
+    validator_stats_output,
 )
 
 load_var_file(EnvironmentVariables.dotenv_file)
@@ -18,9 +18,7 @@ user_home = f'{os.path.expanduser("~")}'
 
 if not environ.get("VALIDATOR_WALLET"):
     while True:
-        wallet = input(
-            "* If you'd like to use stats, we need a one1 or 0x address, please input your address now: "
-        )
+        wallet = input("* If you'd like to use stats, we need a one1 or 0x address, please input your address now: ")
         if wallet.startswith("one1") or wallet.startswith("0x"):
             # Re-enter the wallet to verify
             verify_wallet = input("* Please re-enter your wallet address for verification: ")
