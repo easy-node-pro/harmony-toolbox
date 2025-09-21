c10b8ae8794c1264d229742baa7210c5728d8462
patrickmogul <patrick@easynode.pro>
2025-09-21 15:10:48 -0500

    Remove hardcoded help menu, let argparse handle it dynamically

--- src/menu.py
+++ src/menu.py
@@ -1,17 +1,4 @@
 import sys
-if '-h' in sys.argv or '--help' in sys.argv:
-    print("Harmony Validator Toolbox - Help Menu by EasyNodePro.com")
-    print()
-    print("options:")
-    print("  -h, --help           show this help message and exit")
-    print("  -s, --stats          Run your stats if Harmony is installed and running.")
-    print("  -u, --upgrade        Upgrade your Harmony binary if an upgrade is available.")
-    print("  -c, --collect        Collect your rewards to your validator wallet")
-    print("  -cs, --collect-send  Collect your rewards to your validator wallet and send them to your rewards wallet")
-    print("  -i, --install        Install Harmony ONE and hmy CLI if not installed.")
-    print("  -r, --refresh        Enable auto-refresh mode with 10s interval")
-    sys.exit(0)
-
 import argparse
 import os
 import hashlib
