77850d70fbc1506722710e12ea804abf008461f2
patrickmogul <patrick@easynode.pro>
2025-09-29 22:20:15 -0500

    Fix formatting in validator_stats_output for consistent service status display

--- src/toolbox/utils.py
+++ src/toolbox/utils.py
@@ -489,7 +489,7 @@ def validator_stats_output() -> None:
     print(
         f"* {Fore.CYAN}Service Status & Sync:{Fore.GREEN}\n" +
         f"* {'Folder':<10} {'S':<2} {'Sync':<5} {'Epoch':<6} {'DB 0':<6} {'Free 0':<6} {'DB 1':<6} {'Free 1':<6} {'Local Block':<12}\n"  +
-        f"* {'-'*10} {'-'*2} {'-'*5} {'-'*6} {'-'*6} {'-'*6} {'-'*6} {'-'*6} {'-'*12}\n"
+        f"* {'-'*10} {'-'*2} {'-'*5} {'-'*6} {'-'*6} {'-'*6} {'-'*6} {'-'*6} {'-'*12}"
     )
 
     # Print results for each folder
